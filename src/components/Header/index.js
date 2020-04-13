9639a32ff3936bba3cf86e5c50bc9dfcbf3e2c87
Macie <macie.grant@kikoda.com>
2020-04-12 20:57:38 -0400

    Added initial header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -10,24 +10,28 @@ import {
 } from "reactstrap";
 
 import "./styles.scss";
+import CorbettsMug from "../../Images/corbetts-mug.png";
 
-const Header = () => (
+const Header = (props) => (
   <div>
-    <Card>
-      <CardImg
-        top
-        width="100%"
-        src="/assets/318x180.svg"
-        alt="Card image cap"
-      />
+    <Card body outline color="dark" className="text-center mt-2">
+      <CardImg top width="50%" src={CorbettsMug} alt="Card image cap" />
       <CardBody>
-        <CardTitle>Carry Out &#38; Delivery</CardTitle>
+        <CardTitle>
+          <h1>Carry Out &#38; Delivery</h1>
+        </CardTitle>
         <CardSubtitle>
           BOGO Beer &#38; Wine <span>(with purchase of food)</span>
         </CardSubtitle>
         <CardText>
-          Call <h2>(305) 238-0823</h2> to place a TO-GO Order or click here{" "}
-          <a href="https://www.ubereats.com/miami/food-delivery/corbetts-sports-bar-and-grill/p17_xWy0RZ2zZjz4Qpl0iw"></a>{" "}
+          Call{" "}
+          <h2 className="green">
+            <i className="fa fa-mobile"></i> (305) 238-0823
+          </h2>{" "}
+          to place a TO-GO Order or
+          <a href="https://www.ubereats.com/miami/food-delivery/corbetts-sports-bar-and-grill/p17_xWy0RZ2zZjz4Qpl0iw">
+            click here
+          </a>{" "}
           to place an order with Uber <span className="green">Eats</span>
         </CardText>
         <Button>Button</Button>
