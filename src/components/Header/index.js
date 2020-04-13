c638801a2e52c55cc518d46874ec748c4e64f7f5
Macie <macie.grant@kikoda.com>
2020-04-12 22:03:37 -0400

    Updated Header card

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -13,31 +13,33 @@ import "./styles.scss";
 import CorbettsMug from "../../Images/corbetts-mug.png";
 
 const Header = (props) => (
-  <div>
-    <Card body outline color="dark" className="text-center mt-2">
-      <CardImg top width="50%" src={CorbettsMug} alt="Card image cap" />
-      <CardBody>
-        <CardTitle>
-          <h1>Carry Out &#38; Delivery</h1>
-        </CardTitle>
-        <CardSubtitle>
-          BOGO Beer &#38; Wine <span>(with purchase of food)</span>
-        </CardSubtitle>
-        <CardText>
-          Call{" "}
-          <h2 className="green">
-            <i className="fa fa-mobile"></i> (305) 238-0823
-          </h2>{" "}
-          to place a TO-GO Order or
-          <a href="https://www.ubereats.com/miami/food-delivery/corbetts-sports-bar-and-grill/p17_xWy0RZ2zZjz4Qpl0iw">
-            click here
+  <Card body outline color="dark" className="text-center mt-2">
+    <CardImg top width="50%" src={CorbettsMug} alt="Card image cap" />
+    <CardBody>
+      <CardTitle>
+        <h1>We Are Open For Carry Out &#38; Delivery</h1>
+      </CardTitle>
+      <CardSubtitle>
+        <h4>Buy One Get One Free on all in-stock Beer &#38; Wine </h4>
+        <span>(With any food purchase, while supplies last)</span>
+      </CardSubtitle>
+      <CardText className="mt-2">
+        <p className="mb-1">To Place a Carry Out Order Call</p>
+        <h2 className="green">
+          <i className="fa fa-mobile"></i> (305) 238-0823
+        </h2>{" "}
+        <p>
+          Or Place an Order With{" "}
+          <a
+            href="https://www.ubereats.com/miami/food-delivery/corbetts-sports-bar-and-grill/p17_xWy0RZ2zZjz4Qpl0iw"
+            className="green"
+          >
+            <strong>Uber Eats</strong>
           </a>{" "}
-          to place an order with Uber <span className="green">Eats</span>
-        </CardText>
-        <Button>Button</Button>
-      </CardBody>
-    </Card>
-  </div>
+        </p>
+      </CardText>
+    </CardBody>
+  </Card>
 );
 
 export default Header;
