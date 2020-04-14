67958cd4ca847a4376c15b61f4f4b62c137da912
Macie <macie.grant@kikoda.com>
2020-04-13 20:43:19 -0400

    Moved button into the app

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,8 @@
 import React from "react";
-import { Container } from "reactstrap";
+import { Container, Button } from "reactstrap";
+
+import UrberEats from "../Images/uber-eats.png";
+
 import NavBar from "./NavBar/index";
 import Header from "./Header/index";
 import Footer from "./Footer/index";
@@ -10,6 +13,14 @@ const App = () => {
       <NavBar></NavBar>
       <Container>
         <Header></Header>
+        <div>
+          <Button size="sm">
+            Delivery with
+            <a href="https://www.ubereats.com/miami/food-delivery/corbetts-sports-bar-and-grill/p17_xWy0RZ2zZjz4Qpl0iw">
+              <img src={UrberEats} width="15%"></img>
+            </a>
+          </Button>
+        </div>
         <Footer></Footer>
       </Container>
     </React.Fragment>
