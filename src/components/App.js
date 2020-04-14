44b6233e4ec856a83b7442b37b1b92380652de65
Macie <macie.grant@kikoda.com>
2020-04-14 19:40:26 -0400

    Updated Uber Eats link

--- src/components/App.js
+++ src/components/App.js
@@ -15,12 +15,12 @@ const App = () => {
       <Container>
         <Header></Header>
         <FoodMenu></FoodMenu>
-        <Button size="sm">
-          Delivery with
+        <h2 className="text-center uber-eats-container">
           <a href="https://www.ubereats.com/miami/food-delivery/corbetts-sports-bar-and-grill/p17_xWy0RZ2zZjz4Qpl0iw">
-            <img src={UrberEats} width="15%"></img>
+            Delivery with
+            <img src={UrberEats} width="15%" alt="Uber Eats Logo"></img>
           </a>
-        </Button>
+        </h2>
         <Footer></Footer>
       </Container>
     </React.Fragment>
