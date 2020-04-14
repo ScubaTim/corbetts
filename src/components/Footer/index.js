e3c2ac729b9b40409b7f02b3133338a67e2b81b8
Macie <macie.grant@kikoda.com>
2020-04-13 20:44:00 -0400

    Updated footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,15 +1,15 @@
 import React from "react";
-import UrberEats from "../../Images/uber-eats.png";
 import { Button } from "reactstrap";
 
 import "./styles.scss";
 
 const Footer = (props) => (
-  <div className="footer text-center">
+  <div className="footer text-center p-4">
     <h2>Contact Us</h2>
     <h4>
-      <i class="fa fa-twitter p-2"></i> <i class="fa fa-instagram p-2"></i>{" "}
-      <i class="fa fa-facebook p-2"></i>{" "}
+      <i className="fa fa-twitter p-2"></i>{" "}
+      <i className="fa fa-instagram p-2"></i>{" "}
+      <i className="fa fa-facebook p-2"></i>{" "}
     </h4>
     <h4>
       <i className="fa fa-mobile"></i> (305) 238-0823
@@ -18,18 +18,10 @@ const Footer = (props) => (
       {" "}
       <i className="fa fa-envelope"></i> corbsports@aol.com
     </h4>
-    <div>
-      <Button>
-        Delivery with
-        <a href="https://www.ubereats.com/miami/food-delivery/corbetts-sports-bar-and-grill/p17_xWy0RZ2zZjz4Qpl0iw">
-          <img src={UrberEats} width="20%"></img>
-        </a>
-      </Button>
-    </div>
-    <h4>
-      12721 S Dixie Hwy <br></br>
-      Pinecrest, Florida 33156
-    </h4>
+    <h5>12721 S Dixie Hwy Pinecrest, Florida 33156</h5>
+    <h2>Hours</h2>
+    <h5>Sunday - Thursday 11am - 9pm</h5>
+    <h5>Friday - Saturdau 11am - 10pm</h5>
     <span>Corbett's Sports Bar &copy; 2020</span>
   </div>
 );
